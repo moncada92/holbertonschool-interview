a0e2a67fb8c432669cba1c1c52b894b940eebc16
Camilo Moncada <monk-da@hotmail.com>
2021-01-21 23:00:43 -0500

    hotfix 2

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,61 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: head of the singly linked list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- **/
+ * is_palindrome - check is a palindrome
+ * @head: pointer to head of list
+ * Return: 1: palindrome, 0: not palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t  *left = *head;
-	listint_t *right = *head;
-	int len = 0, num;
+	listint_t *currentS = NULL;
+	listint_t *currentF = NULL;
 
-	left = *head;
-	num = recursive(left, right, len);
-	if (num || *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
-	return (0);
-}
 
-/**
- * recursive - checks if a singly linked list is a palindrome
- * @left: head of the singly linked list
- * @right: tail of the singly linked list
- * @len: value of the singly linked list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- **/
-int recursive(listint_t *left, listint_t *right, int len)
-{
-	int count = 0, num = len;
+	currentS = *head;
+	currentF = *head;
 
-	if (right != NULL)
-	{
-		len++;
-		num = recursive(left, right->next, len);
-		if (num / 2 < len)
-			return (num);
-		if (num == 0)
-			return (0);
-	}
-	else
-		return (num);
-
-
-	while (count < num - len)
-	{
-		left = left->next;
-		count++;
-	}
-
-	if (left->n == right->n)
-	{
-		return (num);
-	}
-	else
+	if (is_palindrome_rec(currentS, currentF) == NULL)
 		return (0);
+	return (1);
+}
+/**
+ * is_palindrome_rec - check is a palindrome recursion
+ * @currentS: pointer to refer the head, the start
+ * @currentF: pointer to move from end to start
+ * Return: pointer: palindrome, NULL: not palindrome
+ */
+listint_t *is_palindrome_rec(listint_t *currentS, listint_t *currentF)
+{
+	listint_t *res = NULL;
+
+	if (currentF != NULL)
+	{
+		res = is_palindrome_rec(currentS, currentF->next);
+		if (res == NULL)
+			return (NULL);
+
+		if (res->n != currentF->n)
+			return (NULL);
+
+		if (res->next == NULL)
+			return (res);
+
+		return (res->next);
+	}
+
+	return (currentS);
 }
