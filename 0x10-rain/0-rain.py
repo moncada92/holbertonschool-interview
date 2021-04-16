40a980fe41ea13c36c463245d83040d390fbe9d4
Camilo Moncada <monk-da@hotmail.com>
2021-04-15 19:54:44 -0500

    hotfix question

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,11 +4,23 @@
 
 def rain(walls):
     """ raun function """
-    if walls is None:
-      return 0
+    if not walls:
+        return 0
 
-    cut_wall = walls[1:-2]
-    count = 0
-    for i in cut_wall:
-        count = count + i
-    return count
+    rainwater = 0
+    for i in range(len(walls)):
+        if walls[i] != max(walls):
+            max_lef = 0
+            max_right = 0
+            for j in range(0, i):
+                if max_lef < walls[j]:
+                    max_lef = walls[j]
+            for k in range(i, len(walls)):
+                if max_right < walls[k]:
+                    max_right = walls[k]
+            if max_lef != 0 and max_right != 0:
+                if max_lef < max_right and max_lef > walls[i]:
+                    rainwater += max_lef - walls[i]
+                elif max_right <= max_lef and max_right > walls[i]:
+                    rainwater += max_right - walls[i]
+    return rainwater
